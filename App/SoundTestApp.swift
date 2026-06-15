f41c7a019b05bf273107f350d70648ee0e856091
Emrah Sümer <sumer@192.168.1.15>
2026-06-15 09:29:45 +0300

    feat: forward scenePhase to audio session manager

--- App/SoundTestApp.swift
+++ App/SoundTestApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import SoundTestCore
 
 @main
 struct SoundTestApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    @StateObject private var audio = AudioSessionManager()
+
     var body: some Scene {
         WindowGroup {
             RootTabView()
+                .environmentObject(audio)
+        }
+        .onChange(of: scenePhase) { _, phase in
+            switch phase {
+            case .active:    audio.handle(.willEnterForeground)
+            case .background: audio.handle(.didEnterBackground)
+            default: break
+            }
         }
     }
 }
